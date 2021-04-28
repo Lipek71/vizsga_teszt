1d40ad6fbaced07dd7c4ab24dfdb8e2abb08369f
Lipek <lipek71@gmail.com>
2021-04-28 22:01:15 +0200

    Teszt2

---
+++ src/main/java/teszt/TesztTwo.java
@@ -0,0 +1,9 @@
+package teszt;
+
+public class TesztTwo {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++){
+            System.out.println(i);
+        }
+    }
+}
